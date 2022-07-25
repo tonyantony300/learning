70a4ca1f64b0ef4886897ba463b6a04c372e8877
TONY ANTONY <tonyantony300@gmail.com>
2022-07-25 21:09:45 +0530

    coding challenge day 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from 'react';
+import './learning.css';
+// import axios from 'axios';
 
-function App() {
+
+// const App = () => {
+
+//   const [photos, setPhotos] = useState([]);
+
+//   useEffect(() => {
+//     axios.get(`https://jsonplaceholder.typicode.com/photos`)
+//     .then(response => {
+//       console.log(response);
+//       setPhotos(response.data)
+//     })
+//   }, [])
+  
+
+//   return (
+//     <ul>{photos.map(photo => <li key={photo.id}>{photo.title}</li>)}</ul>
+//   )
+// }
+
+const App = () =>{
+
+const [counter, setCounter] = useState(0);
+ 
+  //increase counter
+  const increase = () => {
+    setCounter(count => count + 1);
+  };
+ 
+  //decrease counter
+  const decrease = () => {
+    if(counter > 0){
+    setCounter(count => count - 1)};
+  };
+ 
+  //reset counter 
+  const reset = () =>{
+    setCounter(0)
+  }
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="counter">
+      <h1>React Counter</h1>
+      <span className="counter__output">{counter}</span>
+      <div className="btn__container">
+        <button className="control__btn" onClick={increase}>+</button>
+        <button className="control__btn" onClick={decrease}>-</button>
+        <button className="reset" onClick={reset}>Reset</button>
+      </div>
     </div>
   );
-}
+
+
+  }
+
+
 
 export default App;
