4b596f20fd1eafbc8c117594b31b778efccbfed5
TONY ANTONY <tonyantony300@gmail.com>
2022-07-26 17:34:42 +0530

    counter fixed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React,{useState} from 'react';
-import './learning.css';
+// import React,{useState} from 'react';
+// import './learning.css';
 // import axios from 'axios';
 
 
@@ -21,41 +21,105 @@ import './learning.css';
 //   )
 // }
 
-const App = () =>{
+// const App = () =>{
 
-const [counter, setCounter] = useState(0);
+// const [counter, setCounter] = useState(0);
  
-  //increase counter
-  const increase = () => {
-    setCounter(count => count + 1);
-  };
+//   //increase counter
+//   const increase = () => {
+//     setCounter(count => count + 1);
+//   };
  
-  //decrease counter
-  const decrease = () => {
-    if(counter > 0){
-    setCounter(count => count - 1)};
-  };
+//   //decrease counter
+//   const decrease = () => {
+//     if(counter > 0){
+//     setCounter(count => count - 1)};
+//   };
  
-  //reset counter 
-  const reset = () =>{
-    setCounter(0)
+//   //reset counter 
+//   const reset = () =>{
+//     setCounter(0)
+//   }
+ 
+//   return (
+//     <div className="counter">
+//       <h1>React Counter</h1>
+//       <span className="counter__output">{counter}</span>
+//       <div className="btn__container">
+//         <button className="control__btn" onClick={increase}>+</button>
+//         <button className="control__btn" onClick={decrease}>-</button>
+//         <button className="reset" onClick={reset}>Reset</button>
+//       </div>
+//     </div>
+//   );
+
+
+//   }
+
+
+
+// export default App;
+
+// import React from 'react';
+
+
+// class App extends React.Component {
+
+//    state = {seconds: 0 }
+
+//   tick(){
+//     this.setState((prevState)=>({seconds: prevState.seconds+1}))
+//   }
+ 
+//    componentDidMount(){
+//     this.interval = setInterval(()=>this.tick(),100)
+//    }
+
+//   render(){
+//     return (
+//       <div>seconds:{this.state.seconds} </div>
+//     )
+//   }
+// }
+
+// export default App;
+
+import React, { Component } from "react";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { seconds: 10, pause: '' };
+  
   }
- 
-  return (
-    <div className="counter">
-      <h1>React Counter</h1>
-      <span className="counter__output">{counter}</span>
-      <div className="btn__container">
-        <button className="control__btn" onClick={increase}>+</button>
-        <button className="control__btn" onClick={decrease}>-</button>
-        <button className="reset" onClick={reset}>Reset</button>
-      </div>
-    </div>
-  );
-
-
+  tick() {
+    if (this.state.seconds === 0 || this.state.pause === 'pause') 
+    return;
+    else {
+      this.setState(prevState => ({
+        seconds: prevState.seconds - 1
+      }));
+    }
   }
 
 
+
+  componentDidMount() {
+    this.interval = setInterval(() => this.tick(), 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  render() {
+    return (<>
+    <h1>Seconds: {this.state.seconds}</h1>
+    <button onClick={()=> this.setState({pause: 'pause'})}>pause</button>
+    <button onClick={()=>this.setState({seconds: 10, pause: ''})}> reset count</button>
+    </>)
+  }
+}
 
 export default App;
